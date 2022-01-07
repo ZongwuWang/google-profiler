974324271ee1b628a0e846acc2ec91ededa4ded4
zongwuwang <wangzongwu@sjtu.edu.cn>
2022-01-07 18:18:25 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # google-profiler
 Demo for google profiler usage
+
+## How to run?
+
+```shell
+$ source $REPO_HOME/gprof.sh
+```
+
+The pdf results will be genereated in "build/Debug" folder
--- build/Debug/gprof.sh
+++
@@ -1,4 +0,0 @@
-HEAPPROFILE=heapprof ./hello
-pprof -pdf ./hello ./heapprof* > heapprof.pdf
-#CPUPROFILE=cpuprof.out CPUPROFILE_FREQUENCY=1000 ./hello
-#pprof -pdf ./hello ./cpuprof.out > cpuprof.pdf
